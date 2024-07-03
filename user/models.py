12d42d24103636de7d9fedff4f95c07c91f55d91
dudxor4587 <dudxor4587@gmail.com>
2024-07-03 23:31:16 +0900

    fix: erd 수정에 따른 모델 변경

--- user/models.py
+++ user/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 
-class User(models.Model):
-    user_number = models.AutoField(primary_key=True)
-    user_id = models.CharField(max_length=100)
+class user(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    user_email = models.CharField(max_length=100)
     user_name = models.CharField(max_length=100)
-    password = models.CharField(max_length=200)
+    user_password = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return self.user_name
