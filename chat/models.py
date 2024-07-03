a21abd01bdef244fa301ced87289e57939fef207
dudxor4587 <dudxor4587@gmail.com>
2024-07-03 14:34:33 +0900

    refactor: ChatRoom 필드명 수정

--- chat/models.py
+++ chat/models.py
@@ -39,10 +39,10 @@ class Episode(models.Model):
 class ChatRoom(models.Model):
     room_id = models.AutoField(primary_key=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    character = models.ForeignKey(Character, on_delete=models.CASCADE, null=True)
+    character = models.ForeignKey(Character, on_delete=models.CASCADE)
     work = models.ForeignKey(Work, on_delete=models.CASCADE)
     result = models.CharField(max_length=2000, null=True, blank=True)
-    mz_rank = models.IntegerField(null=True, blank=True)
+    mz_percent = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return str(self.room_id)
