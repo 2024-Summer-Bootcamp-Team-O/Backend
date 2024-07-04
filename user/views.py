e3b0f8096bf23a6872e61d88970168372ca21ae3
dudxor4587 <dudxor4587@gmail.com>
2024-07-05 01:56:39 +0900

    fix: 불필요한 import문 제거

--- user/views.py
+++ user/views.py
@@ -2,7 +2,7 @@ from user.models import member
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 from .serializers import UserRegistrationSerializer
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import logout
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
