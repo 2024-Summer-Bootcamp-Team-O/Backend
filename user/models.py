219ab07580553d3650277e01cd44cd493b81b0ed
dudxor4587 <dudxor4587@gmail.com>
2024-07-04 17:51:43 +0900

    fix: 모델 이름 수정 및 celery 재설정

--- user/models.py
+++ user/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 
 
-class user(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    user_email = models.CharField(max_length=100)
-    user_name = models.CharField(max_length=100)
-    user_password = models.CharField(max_length=200)
+class member(models.Model):
+    member_id = models.AutoField(primary_key=True)
+    member_email = models.CharField(max_length=100)
+    member_name = models.CharField(max_length=100)
+    member_password = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return self.user_name
+        return self.member_name
