51e681c461316b45098bc3eb18bc53490128e3f7
SRASONY <leeseula0123@gmail.com>
2024-07-10 21:50:21 +0900

    feat: 대화 시작 시 캐릭터 정보를 저장하고 상황을 랜덤 배정하는 기능 추가

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,4 +1,20 @@
 from rest_framework import serializers
+from .models import chat_room,character,user
+
+class ChatRoomSerializer(serializers.ModelSerializer):
+    character_id = serializers.IntegerField(write_only=True)
+    user_id = serializers.IntegerField(write_only=True)
+
+    class Meta:
+        model = chat_room
+        fields = ['user_id','character_id']
+    def create(self, validated_data):
+        character_id = validated_data.pop('character_id')
+        user_id = validated_data.pop('user_id')
+        character_instance = character.objects.get(character_id=character_id)
+        user_instance = user.objects.get(id =user_id)
+        chat_room_instance = chat_room.objects.create(character=character_instance, user=user_instance, **validated_data)
+        return chat_room_instance
 
 
 class AnswerSerializer(serializers.Serializer):
--- chat/urls.py
+++ chat/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-
+from .views import CreateChatRoomView
 from . import views
 
 appname = "chat"
 
 urlpatterns = [
     path('answers', views.AnswerView.as_view(), name='choice'),
+    path('start', CreateChatRoomView.as_view(), name='start_chat')
 ]
