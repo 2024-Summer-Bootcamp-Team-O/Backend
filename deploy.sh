01b373dc696608254ba0a4f0a3de20caf9efbda8
dudxor4587 <dudxor4587@gmail.com>
2024-07-22 20:55:44 +0900

    fix: 무중단 배포 수정
    Signed-off-by: dudxor4587 <dudxor4587@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,8 @@
 # Variables
 BLUE_COMPOSE_FILE=docker-compose-blue.prod.yml
 GREEN_COMPOSE_FILE=docker-compose-green.prod.yml
-NGINX_CONFIG_FILE=nginx.conf
 VERSION_FILE=current_version.txt
 
-# Read current version from the file
 if [ -f $VERSION_FILE ]; then
     CURRENT_VERSION=$(cat $VERSION_FILE)
 else
@@ -19,6 +17,8 @@ if [ "$CURRENT_VERSION" == "blue" ]; then
     OLD_COMPOSE_FILE=$BLUE_COMPOSE_FILE
     NEW_SERVICE="backend_green"
     OLD_SERVICE="backend_blue"
+    NEW_NGINX_CONFIG="nginx-green.conf"
+    OLD_NGINX_CONFIG="nginx-blue.conf"
     NEW_PORT=8001
     OLD_PORT=8000
 else
@@ -27,22 +27,30 @@ else
     OLD_COMPOSE_FILE=$GREEN_COMPOSE_FILE
     NEW_SERVICE="backend_blue"
     OLD_SERVICE="backend_green"
+    NEW_NGINX_CONFIG="nginx-blue.conf"
+    OLD_NGINX_CONFIG="nginx-green.conf"
     NEW_PORT=8000
     OLD_PORT=8001
 fi
 
 # Build and deploy new version
+echo "Building and deploying $NEW_SERVICE..."
 docker-compose -f $NEW_COMPOSE_FILE build $NEW_SERVICE
 docker-compose -f $NEW_COMPOSE_FILE up -d $NEW_SERVICE
 
-# Update Nginx configuration
-sed -i "s|proxy_pass http://.*:800[0-1];|proxy_pass http://$NEW_SERVICE:$NEW_PORT;|g" $NGINX_CONFIG_FILE
+# Copy new Nginx configuration to container and reload
+echo "Reloading Nginx configuration..."
+docker-compose -f $NEW_COMPOSE_FILE down nginx
+docker-compose -f $NEW_COMPOSE_FILE up -d nginx
 
-# Reload Nginx to apply new configuration
 docker-compose -f $NEW_COMPOSE_FILE exec nginx nginx -s reload
 
 # Stop old version
+echo "Stopping old version $OLD_SERVICE..."
 docker-compose -f $OLD_COMPOSE_FILE stop $OLD_SERVICE
 
 # Update version file
+echo "Updating version file..."
 echo $NEW_VERSION > $VERSION_FILE
+
+echo "Deployment completed successfully!"
