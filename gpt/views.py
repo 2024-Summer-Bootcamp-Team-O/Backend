0a4d60cf86d4f52c39021e5c255038ac5d17c4da
dudxor4587 <dudxor4587@gmail.com>
2024-07-24 04:48:00 +0900

    fix: redis key값을 각 사용자가 고유하게 가지도록 수정
    Signed-off-by: dudxor4587 <dudxor4587@gmail.com>

--- gpt/views.py
+++ gpt/views.py
@@ -19,21 +19,22 @@ class GetGPTMessageView(APIView):
         operation_id="GPT의 대사를 가져오는 API",
     )
     def post(self, request):
+        user_email = request.user.email
         serializer = GetGPTMessageSerializer(data=request.data)
         if serializer.is_valid():
             character_id = serializer.validated_data.get("character_id")
             episode_id = serializer.validated_data.get("episode_id")
 
             r = redis.Redis(host="redis", port=6379, db=0)
-            if r.exists("episode_id"):
-                r.delete("episode_id")
-            r.set("episode_id", episode_id)
-            if r.exists("count"):
-                r.incr("count")
+            if r.exists(f"episode_id:{user_email}"):
+                r.delete(f"episode_id:{user_email}")
+            r.set(f"episode_id:{user_email}", episode_id)
+            if r.exists(f"count:{user_email}"):
+                r.incr(f"count:{user_email}")
             else:
-                r.set("count", 1)
-            r.set("character_id", character_id)
-            result = get_gpt_message.delay(character_id, episode_id)
+                r.set(f"count:{user_email}", 1)
+            r.set(f"character_id:{user_email}", character_id)
+            result = get_gpt_message.delay(character_id, episode_id, user_email)
             return JsonResponse(
                 {"task_id": result.task_id}, status=status.HTTP_202_ACCEPTED
             )
@@ -49,7 +50,8 @@ class GetGPTFeedbackView(APIView):
         operation_id="GPT의 피드백을 가져오는 API",
     )
     def get(self, request):
-        result = get_gpt_feedback.delay()
+        user_email = request.user.id
+        result = get_gpt_feedback.delay(user_email)
         return JsonResponse(
             {"task_id": result.task_id}, status=status.HTTP_202_ACCEPTED
         )
@@ -63,6 +65,7 @@ class GetGPTResultView(APIView):
         operation_id="GPT의 최종 피드백을 가져오는 API",
     )
     def get(self, request):
-        reponse = get_gpt_result()
+        user_email = request.user.id
+        reponse = get_gpt_result(user_email)
         result = {"result": reponse}
         return JsonResponse(result, status=status.HTTP_200_OK)
